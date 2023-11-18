7ef72b864c21b253a8e684281707ac8ad268b8f5
Martin Fink <martin@finkmartin.com>
2023-11-19 00:02:32 +0100

    Change default tab

--- Percipe/Views/HomeView.swift
+++ Percipe/Views/HomeView.swift
@@ -8,20 +8,25 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var selection = 1
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selection) {
             DiscoverView()
                 .tabItem {
                     Label("Discover", systemImage: "magnifyingglass")
                 }
+                .tag(0)
             RecipesList()
                 .tabItem {
                     Label("Recipes", systemImage: "list.bullet")
                 }
+                .tag(1)
             ProfileView()
                 .tabItem {
                     Label("Profile", systemImage: "person.crop.circle")
                 }
+                .tag(2)
         }.toolbarTitleDisplayMode(.inline)
     }
 }
