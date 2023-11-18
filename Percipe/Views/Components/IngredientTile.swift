d8f5832ddebbe4a3e10c57e849ebc64725c47d5c
Martin Fink <martin@finkmartin.com>
2023-11-18 20:47:34 +0100

    Add warning to ingredient

--- Percipe/Views/Components/IngredientTile.swift
+++ Percipe/Views/Components/IngredientTile.swift
@@ -11,30 +11,56 @@ struct IngredientTile: View {
     let name: String
     let amount: String
     let imageUrl: String
+    let isAllergy: Bool
+    
+    init(name: String, amount: String, imageUrl: String, isAllergy: Bool = false) {
+        self.name = name
+        self.amount = amount
+        self.imageUrl = imageUrl
+        self.isAllergy = isAllergy
+    }
+    
+    var color: Color {
+        if self.isAllergy {
+            .red
+        } else {
+            .black
+        }
+    }
     
     var body: some View {
-        VStack {
-            Spacer()
-            VStack(alignment: .leading) {
-                HStack {
-                    Spacer()
-                }
-                Text(name)
-                    .lineLimit(2)
+        ZStack(alignment: .topLeading) {
+            VStack {
+                Spacer()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Spacer()
+                    }
+                    Text(name)
+                        .lineLimit(2)
                     //.truncationMode(.tail)
-                    .padding(.top, 30)
-                    .fontWeight(.semibold)
-                Text(amount)
-                    .lineLimit(1)
-                    .truncationMode(.tail)
-                    .padding(.bottom, 5)
+                        .padding(.top, 30)
+                        .fontWeight(.semibold)
+                    Text(amount)
+                        .lineLimit(1)
+                        .truncationMode(.tail)
+                        .padding(.bottom, 5)
+                }
+                .padding(.horizontal, 5)
+                .frame(maxWidth: .infinity)
+                .background(LinearGradient(colors: [
+                    self.color.opacity(0.0),
+                    self.color.opacity(0.4),
+                ], startPoint: .top, endPoint: .bottom))
             }
-            .padding(.horizontal, 5)
-            .frame(maxWidth: .infinity)
-            .background(LinearGradient(colors: [
-                Color.black.opacity(0.0),
-                Color.black.opacity(0.4),
-            ], startPoint: .top, endPoint: .bottom))
+            if self.isAllergy {
+                RoundedRectangle(cornerRadius: 6)
+                    .stroke(.red, lineWidth: 2)
+                Image(systemName: "exclamationmark.triangle")
+                    .foregroundColor(.red)
+                    .font(.title)
+                    .padding(10)
+            }
         }
         .foregroundColor(.white)
         .frame(width: 100, height: 150)
@@ -51,10 +77,13 @@ struct IngredientTile: View {
             )
         )
         .cornerRadius(6)
-        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+        .shadow(radius: 10)
     }
 }
 
 #Preview {
-    IngredientTile(name: "Parsley", amount: "10 g", imageUrl: "https://img.hellofresh.com/w_1024,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/64da29a5796d73553a89656b-a8fa0db5.png")
+    HStack {
+        IngredientTile(name: "Parsley", amount: "10 g", imageUrl: "https://img.hellofresh.com/w_1024,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/64da29a5796d73553a89656b-a8fa0db5.png")
+        IngredientTile(name: "Parsley", amount: "10 g", imageUrl: "https://img.hellofresh.com/w_1024,q_auto,f_auto,c_limit,fl_lossy/hellofresh_s3/ingredient/64da29a5796d73553a89656b-a8fa0db5.png", isAllergy: true)
+    }
 }
