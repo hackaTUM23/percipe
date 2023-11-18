2338cda6e005925a1ceef0f34023ac5c91624815
Andreas Resch <andreas@resch.io>
2023-11-18 10:39:38 +0100

    add compiling project template

---
+++ Percipe/PercipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PercipeApp.swift
+//  Percipe
+//
+//  Created by Andreas Resch on 18.11.23.
+//
+
+import SwiftUI
+
+@main
+struct PercipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
